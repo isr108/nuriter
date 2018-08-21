46635996fc4e55bff84e1e551cb1b920e209f4d2
user1 <user1@KH_B>
2018-08-21 11:16:23 +0900

    결제 고고

---
+++ nuriter-semi/src/com/kh/nuriter/payment/model/vo/Payment.java
@@ -0,0 +1,53 @@
+package com.kh.nuriter.payment.model.vo;
+
+public class Payment {
+	private String pId; //결제아이디
+	private int userNum;//회원번호
+	private String nuriNum;//누리터 번호
+	
+	public Payment(){}
+
+	public Payment(String pId, int userNum, String nuriNum) {
+		super();
+		this.pId = pId;
+		this.userNum = userNum;
+		this.nuriNum = nuriNum;
+	}
+
+	public String getpId() {
+		return pId;
+	}
+
+	public int getUserNum() {
+		return userNum;
+	}
+
+	public String getNuriNum() {
+		return nuriNum;
+	}
+
+	public void setpId(String pId) {
+		this.pId = pId;
+	}
+
+	public void setUserNum(int userNum) {
+		this.userNum = userNum;
+	}
+
+	public void setNuriNum(String nuriNum) {
+		this.nuriNum = nuriNum;
+	}
+
+	@Override
+	public String toString() {
+		return "Payment [pId=" + pId + ", userNum=" + userNum + ", nuriNum=" + nuriNum + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
